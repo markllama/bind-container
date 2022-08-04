227657c4a190f60b3acf05b1b2f18a155e47e4f0
Mark Lamourine <markllama@gmail.com>
2022-08-04 20:52:32 +0000

    switched to fedora minmal, microdnf and use simpler entrypoint spec

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
 set -x
-
+MAINTAINER='markllama@gmail.com'
 CONTAINER_NAME=$1
-BASE_IMAGE=registry.access.redhat.com/ubi8/ubi
+#BASE_IMAGE=registry.access.redhat.com/ubi8/ubi
+BASE_IMAGE=registry.fedoraproject.org/fedora-minimal:36
 IMAGE_NAME=$2
 
 RPMS=(bind bind-utils)
@@ -12,20 +12,14 @@ SERVICES=(named)
 # from fedora
 buildah from --name ${CONTAINER_NAME} ${BASE_IMAGE}
 buildah config --label maintainer="${MAINTAINER}" ${CONTAINER_NAME}
+#buildah config --env INTERFACE=eno1 ${CONTAINER_NAME}
 
-buildah config --env INTERFACE=eno1 ${CONTAINER_NAME}
-
-#buildah config --port 68/tcp,68/udp,69/tcp,69/udp ${CONTAINER_NAME}
-
-buildah run ${CONTAINER_NAME} dnf -y install ${RPMS[@]}
-#buildah run ${CONTAINER_NAME} systemctl enable dhcpd
-buildah run ${CONTAINER_NAME} dnf clean all
+buildah run ${CONTAINER_NAME} microdnf -y install ${RPMS[@]}
+buildah run ${CONTAINER_NAME} microdnf clean all
 
 # copy entrypoint.sh
 buildah copy ${CONTAINER_NAME} entrypoint.sh /opt/entrypoint.sh
-
-# set entrypoint
-buildah config --entrypoint '["/opt/entrypoint.sh"]' ${CONTAINER_NAME}
+buildah config --entrypoint '/opt/entrypoint.sh' ${CONTAINER_NAME}
 
 buildah commit ${CONTAINER_NAME} ${IMAGE_NAME}
 
